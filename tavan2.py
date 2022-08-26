c42d0b83f87ebf75f1560695d27f02afc0e8c4ee
Fatemeh-Zangooie-roshnavand <110530784+fatemehzangooie@users.noreply.github.com>
2022-08-26 11:48:16 -0700

    Add files via upload

---
+++ tavan2.py
@@ -0,0 +1,11 @@
+from random import random
+
+
+import math
+list1=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+list2=[]
+for i in range(len(list1)):
+    list2.append(math.pow(list1[i],2))
+    print("list frome 1 to 20: ",list1)
+    print("list frome 1 to 20 to power of 2: ".list2)
+    
